2df7f034342d497d93617d8b12aea1ac3dee80fa
lulee01 <luka121001@gmail.com>
2023-05-24 14:51:27 +0200

    Komentari za  Add Item to Cart test

--- Tests/AddItemToCartTest.cs
+++ Tests/AddItemToCartTest.cs
@@ -7,30 +7,44 @@ namespace AutomationFramework.Tests
         [SetUp]
         public void SetUp()
         {
+            ///Klik na Login link
             Pages.IndexPage.ClickOnLoginOrRegister();
 
+            //Unos podataka u fomru za login
             Pages.AccountPage.LoginUser(
                 TestData.User.Login.username,
                 TestData.User.Login.password);
+
+            //Klik na link korpe
             Pages.IndexPage.ClickOnCartPageLink();
+
+            //Brisanje proizvoda iz korpe
             Pages.CartPage.RemoveItemsFromCart();
         }
 
         [Test]
         public void AddFromIndexPage()
         {
+            //Dodavanje proizvoda u korpu sa indeks strane(Home strane)
             Pages.IndexPage.AddProductFromIndex();
+
+            //Klik na link korpe
             Pages.IndexPage.ClickOnCartPageLink();
-            // Asertacija
+
+            // Asertacija(Provera da li je korpa prazna)
             Assert.IsTrue(Pages.CartPage.IsCartNotEmpty());
         }
 
         [Test]
         public void AddFromProductPage()
         {
+            //Klik na proizvod
             Pages.IndexPage.ClickOnProduct();
+
+            //Dodavanje proizvoda u korpu
             Pages.ProductPage.ClickOnAddToCart();
-            //Asertacija 
+
+            //Asertacija (Provera da li je korpa prazna)
             Assert.IsTrue(Pages.CartPage.IsCartNotEmpty());
         }
     }
